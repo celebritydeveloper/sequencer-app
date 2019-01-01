4d6a787bdb7fca510e56296e3924cca6ebfea877
Essien Saviour <IbibioDeveloper@Wisdomanthonis-MacBook-Pro-2.local>
2019-01-01 12:21:48 +0100

    Updates

---
+++ precache-manifest.cd47ce650b6a66c70bc5756b1bf39980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f12dc495044a92bfcb6",
+    "url": "/react-gh-pages/static/css/main.9b62f9cb.chunk.css"
+  },
+  {
+    "revision": "9f12dc495044a92bfcb6",
+    "url": "/react-gh-pages/static/js/main.9f12dc49.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d48476d78e82172149c7056bc5a9346",
+    "url": "/react-gh-pages/index.html"
+  }
+];
