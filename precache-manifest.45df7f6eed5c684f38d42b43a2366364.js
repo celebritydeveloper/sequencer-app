06ca3018d2fd5eeac14aee76692c2daf91a89617
Essien Saviour <IbibioDeveloper@Wisdomanthonis-MacBook-Pro-2.local>
2019-01-01 12:52:00 +0100

    Updates

---
+++ precache-manifest.45df7f6eed5c684f38d42b43a2366364.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bbc300af2eb6f7182f7",
+    "url": "/react-gh-pages/static/css/main.b3dfdac4.chunk.css"
+  },
+  {
+    "revision": "1bbc300af2eb6f7182f7",
+    "url": "/react-gh-pages/static/js/main.1bbc300a.chunk.js"
+  },
+  {
+    "revision": "ae7f464fdf4abeef94e9",
+    "url": "/react-gh-pages/static/css/1.26ccc908.chunk.css"
+  },
+  {
+    "revision": "ae7f464fdf4abeef94e9",
+    "url": "/react-gh-pages/static/js/1.ae7f464f.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "b185066a39fd4d19e78c2d69d8cedfef",
+    "url": "/react-gh-pages/static/media/sequence.b185066a.jpeg"
+  },
+  {
+    "revision": "4e7bc778787655d59954fce93e6cd04c",
+    "url": "/react-gh-pages/index.html"
+  }
+];
